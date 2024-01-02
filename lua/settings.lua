abb9d3948ba2ebfcf1699075b245b0466a12e68e
Andrei Vasilev <amlk158@gmail.com>
2024-01-02 20:38:16 +0200

    initial neovim setup with lua

---
+++ lua/settings.lua
@@ -0,0 +1,52 @@
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+--
+local g = vim.g
+local o = vim.o
+
+
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+-- Set highlight on search
+o.hlsearch = false
+
+-- Make line numbers default
+vim.wo.number = true
+
+o.relativenumber = true
+
+-- Enable mouse mode
+o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+o.breakindent = true
+
+-- Save undo history
+o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+o.updatetime = 250
+o.timeout = true
+o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+o.termguicolors = true
+
+vim.opt.guifont = {'JetBrains_Mono', ':h15'}
